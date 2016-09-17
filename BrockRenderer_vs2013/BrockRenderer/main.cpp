8744721d528f9969a9b2d79052b7a15d896d8aa8
lyk <371162195@qq.com>
2016-09-17 12:04:53 +0800

    code reorganize
    Signed-off-by: lyk <371162195@qq.com>

--- BrockRenderer_vs2013/BrockRenderer/main.cpp
+++ BrockRenderer_vs2013/BrockRenderer/main.cpp
@@ -1,12 +1,18 @@
 #include "stdafx.h"
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, char *, int cmdShow)
+
+void init()
 {
 	g_scene = new Scene();
 	g_scene->LoadModel("Data/cj6.obj");
 	g_timer = new Timer();
 	g_camera = new Camera();
 	g_context = new Context();
+}
+int WINAPI WinMain(HINSTANCE, HINSTANCE, char *, int cmdShow)
+{
+	init();
+	// render loop begins
 	WinApp::Instance()->run();
 	return 0;
 }
